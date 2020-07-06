f6a8056ccafe015f494b6f5761291408bc777e80
kinawy2001 <mahmmoudkinawy@gmail.com>
2020-07-06 18:56:08 +0200

    ADD simple calculater

---
+++ Simple calculator using java/src/clearcalc/ClearCalc.java
@@ -0,0 +1,12 @@
+package clearcalc;
+
+public class ClearCalc {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        calc lol = new calc();
+        lol.setVisible(true);
+    }
+    
+}
